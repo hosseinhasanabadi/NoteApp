60f799793e022297dc52171c043ad7ce0ddf62c7
Hossein <hhasnabadi@yahoo.com>
2024-08-14 23:58:52 +0330

    NoteApp

---
+++ app/src/main/java/com/example/noteappe/deta/NoteData.kt
@@ -0,0 +1,24 @@
+package com.example.noteappe.deta
+
+import com.example.noteappe.model.Note
+
+class NoteDataSours{
+    fun loadNote(): List<Note>{
+        return listOf(
+            Note(title = "A good day", description = "We went on a vacation by the lake"),
+            Note(title = "Android Compose", description = "Working on Android Compose course today"),
+            Note(title = "Keep at it...", description = "Sometimes things just happen"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family")
+        )
+
+
+
+    }
+
+}
